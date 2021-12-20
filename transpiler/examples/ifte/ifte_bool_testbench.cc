4110ec65240bc976a35bc979b5c0c89781bff51b
Iliyan Malchev <malchev@google.com>
2021-12-20 14:03:30 -0800

    Add a simple ifte example (if-then-else).
    Selects betweent two 8-bit values based on a single-bit condition.
    
    e.g. bazel run transpiler/examples/ifte:ifte . a b # expect 'b'
    
    PiperOrigin-RevId: 417475347

---
+++ transpiler/examples/ifte/ifte_bool_testbench.cc
@@ -0,0 +1,72 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#include <stdint.h>
+
+#include <iostream>
+
+#include "transpiler/data/boolean_data.h"
+#include "xls/common/logging/logging.h"
+
+#ifdef USE_YOSYS_PLAINTEXT
+#include "transpiler/examples/ifte/ifte_yosys_plaintext.h"
+#else
+#include "transpiler/examples/ifte/ifte_bool.h"
+#endif
+
+int main(int argc, char** argv) {
+  if (argc < 4) {
+    std::cerr << "usage: ifte i t e" << std::endl;
+    return 1;
+  }
+
+  char i = argv[1][0];
+  if (i == '.') i = 0;
+  char t = argv[2][0];
+  char e = argv[3][0];
+
+  std::cout << "i: " << i << std::endl;
+  std::cout << "t: " << t << std::endl;
+  std::cout << "e: " << e << std::endl;
+
+  // Encrypt data
+  EncodedBool encoded_i;
+  encoded_i.Encode(!!i);
+  EncodedChar encoded_t;
+  encoded_t.Encode(t);
+  EncodedChar encoded_e;
+  encoded_e.Encode(e);
+
+  std::cout << "Encryption done" << std::endl;
+
+  std::cout << "Initial state check by decryption: " << std::endl;
+  // Decrypt results.
+  std::cout << static_cast<char>(encoded_i.Decode());
+  std::cout << "  ";
+  std::cout << static_cast<char>(encoded_t.Decode());
+  std::cout << "  ";
+  std::cout << static_cast<char>(encoded_e.Decode());
+
+  std::cout << "\n";
+
+  std::cout << "\t\t\t\t\tServer side computation:" << std::endl;
+  // Perform addition
+  EncodedChar cipher_result;
+  XLS_CHECK_OK(ifte(cipher_result.get(), encoded_i.get(), encoded_t.get(),
+                    encoded_e.get()));
+
+  std::cout << "\t\t\t\t\tComputation done" << std::endl;
+
+  std::cout << "Decrypted result: " << (char)cipher_result.Decode() << "\n";
+  std::cout << "Decryption done" << std::endl;
+}
---
+++ transpiler/examples/ifte/ifte_tfhe_testbench.cc
@@ -0,0 +1,89 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#include <stdint.h>
+
+#include <iostream>
+
+#include "tfhe/tfhe.h"
+#include "transpiler/data/fhe_data.h"
+#include "xls/common/logging/logging.h"
+
+#ifdef USE_INTERPRETED_TFHE
+#include "transpiler/examples/ifte/ifte_interpreted_tfhe.h"
+#elif USE_YOSYS_INTERPRETED_TFHE
+#include "transpiler/examples/ifte/ifte_yosys_interpreted_tfhe.h"
+#else
+#include "transpiler/examples/ifte/ifte_tfhe.h"
+#endif
+
+const int main_minimum_lambda = 120;
+
+int main(int argc, char** argv) {
+  if (argc < 4) {
+    std::cerr << "usage: ifte i t e" << std::endl;
+    return 1;
+  }
+
+  char i = argv[1][0];
+  if (i == '.') i = 0;
+  char t = argv[2][0];
+  char e = argv[3][0];
+
+  // generate a keyset
+  TFheGateBootstrappingParameterSet* params =
+      new_default_gate_bootstrapping_parameters(main_minimum_lambda);
+
+  // generate a random key
+  // Note: In real applications, a cryptographically secure seed needs to be
+  // used.
+  uint32_t seed[] = {314, 1592, 657};
+  tfhe_random_generator_setSeed(seed, 3);
+  TFheGateBootstrappingSecretKeySet* key =
+      new_random_gate_bootstrapping_secret_keyset(params);
+  const TFheGateBootstrappingCloudKeySet* cloud_key = &key->cloud;
+
+  std::cout << "i: " << i << std::endl;
+  std::cout << "t: " << t << std::endl;
+  std::cout << "e: " << e << std::endl;
+
+  // Encrypt data
+  auto ciphertext_i = FheBool::Encrypt(i, key);
+  auto ciphertext_t = FheChar::Encrypt(t, key);
+  auto ciphertext_e = FheChar::Encrypt(e, key);
+
+  std::cout << "Encryption done" << std::endl;
+
+  std::cout << "Initial state check by decryption: " << std::endl;
+  // Decrypt results.
+  std::cout << static_cast<char>(ciphertext_i.Decrypt(key));
+  std::cout << "  ";
+  std::cout << static_cast<char>(ciphertext_t.Decrypt(key));
+  std::cout << "  ";
+  std::cout << static_cast<char>(ciphertext_e.Decrypt(key));
+
+  std::cout << "\n";
+
+  std::cout << "\t\t\t\t\tServer side computation:" << std::endl;
+  // Perform addition
+  FheChar cipher_result(params);
+  XLS_CHECK_OK(ifte(cipher_result.get(), ciphertext_i.get(), ciphertext_t.get(),
+                    ciphertext_e.get(), cloud_key));
+
+  std::cout << "\t\t\t\t\tComputation done" << std::endl;
+
+  char res = cipher_result.Decrypt(key);
+  std::cout << "Decrypted result: " << res << " (hex " << std::hex << (int)res
+            << ")" << std::endl;
+  std::cout << "Decryption done" << std::endl;
+}
