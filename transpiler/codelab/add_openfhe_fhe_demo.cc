f8ac98cbee69be3340c22b3e6f5b89b48d7189a5
Jeremy Kun <jkun@google.com>
2022-10-24 10:51:22 -0700

    add a simple addition example
    PiperOrigin-RevId: 483428110

---
+++ transpiler/codelab/add_openfhe_fhe_demo.cc
@@ -0,0 +1,50 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <stdio.h>
+
+#include <iostream>
+#include <ostream>
+
+#include "absl/strings/numbers.h"
+#include "transpiler/codelab/add_fhe_lib.h"
+#include "transpiler/data/openfhe_data.h"
+
+constexpr auto kSecurityLevel = lbcrypto::MEDIUM;
+
+int main(int argc, char** argv) {
+  if (argc < 3) {
+    fprintf(stderr, "Usage: add_main [int] [int]\n\n");
+    return 1;
+  }
+
+  int x, y;
+  XLS_CHECK(absl::SimpleAtoi(argv[1], &x));
+  XLS_CHECK(absl::SimpleAtoi(argv[2], &y));
+  std::cout << "Computing " << x << " + " << y << std::endl;
+
+  // Set up backend context and encryption keys.
+  auto context = lbcrypto::BinFHEContext();
+  context.GenerateBinFHEContext(kSecurityLevel);
+  auto sk = context.KeyGen();
+  context.BTKeyGen(sk);
+
+  auto ciphertext_x = OpenFhe<signed int>::Encrypt(x, context, sk);
+  auto ciphertext_y = OpenFhe<signed int>::Encrypt(y, context, sk);
+  OpenFhe<signed int> result(context);
+  XLS_CHECK_OK(add(result, ciphertext_x, ciphertext_y, context));
+
+  std::cout << "Result: " << result.Decrypt(sk) << "\n";
+  return 0;
+}
---
+++ transpiler/codelab/add_test.cc
@@ -0,0 +1,34 @@
+// Copyright 2021 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "gtest/gtest.h"
+#include "transpiler/codelab/add_fhe_lib.h"
+#include "transpiler/data/openfhe_data.h"
+
+TEST(AddOpenFheTest, Sum) {
+  int x = 5, y = 7;
+
+  auto context = lbcrypto::BinFHEContext();
+  context.GenerateBinFHEContext(lbcrypto::MEDIUM);
+  auto sk = context.KeyGen();
+  context.BTKeyGen(sk);
+
+  auto ciphertext_x = OpenFhe<signed int>::Encrypt(x, context, sk);
+  auto ciphertext_y = OpenFhe<signed int>::Encrypt(y, context, sk);
+  OpenFhe<signed int> result(context);
+  XLS_CHECK_OK(add(result, ciphertext_x, ciphertext_y, context));
+
+  int result_plaintext = result.Decrypt(sk);
+  EXPECT_EQ(result_plaintext, 12);
+}
